8e94c405bd766752e57c0c5e6ce81f8757612801
brido4125 <hcs4125@gmail.com>
2022-10-24 23:15:18 +0900

    update:Insertion Root Node

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,11 +1,19 @@
 #include "rbtree.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
-rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
+node_t* getGrandParent(node_t* node);
+node_t* getUncle(node_t* node);
+void rotate_left(node_t* node);
+void rotate_right(node_t* node);
+
+rbtree* new_rbtree(void) {
+    rbtree *p = (rbtree *) calloc(1, sizeof(rbtree));
   // TODO: initialize struct if needed
-  return p;
+    p->root = NULL;
+    p->nil = NULL;
+    return p;
 }
 
 void delete_rbtree(rbtree *t) {
@@ -13,9 +21,70 @@ void delete_rbtree(rbtree *t) {
   free(t);
 }
 
+node_t* getGrandParent(node_t* node){
+    if ((node != NULL) && (node->parent != NULL)) {
+        return node->parent->parent;
+    } else {
+        return NULL;
+    }
+}
+
+node_t* getUncle(node_t* node){
+    node_t* grand = getGrandParent(node);
+    if (grand == NULL) {
+        return NULL;
+    }
+    if (node->parent == grand->left) {
+        return grand->right;
+    } else{
+        return grand->left;
+    }
+}
+
+
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
-  return t->root;
+    node_t *parentNode = NULL;
+    node_t *newNode = NULL;
+    if (t == NULL){
+        return NULL;
+    }
+    parentNode = t->root;
+    while (parentNode != NULL) {
+        if (key < parentNode->key) {
+            if (parentNode->left == NULL) {
+                break;
+            }else {
+                parentNode = parentNode->left;
+
+            }
+        }else{
+            if (parentNode->right == NULL) {
+                break;
+            } else {
+                parentNode = parentNode->right;
+            }
+        }
+    }
+    newNode = calloc(1, sizeof(node_t));
+    if (newNode != NULL) {
+        newNode->key = key;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->color = RBTREE_RED;
+        if (parentNode == NULL) {
+            printf("Root node add here\n");
+            printf("Root node's Color %d\n",newNode->color);
+            t->root = newNode;
+        }else{
+            if (newNode->key < parentNode->key) {
+                parentNode->left = newNode;
+            }else{
+                parentNode->right = newNode;
+            }
+        }
+    }
+  return newNode;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
